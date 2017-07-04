bd7e7bb58d8e249d46558b19539c99cb2809d23a
Deepak Surti <dmsurti@gmail.com>
2017-07-04 18:09:50 +0530

    Convert to reynolds addon package

---
+++ __init__.py
@@ -0,0 +1,53 @@
+#------------------------------------------------------------------------------
+# Reynolds-Blender | The Blender add-on for Reynolds, an OpenFoam toolbox.
+#------------------------------------------------------------------------------
+# Copyright|
+#------------------------------------------------------------------------------
+#     Deepak Surti       (dmsurti@gmail.com)
+#     Prabhu R           (IIT Bombay, prabhu@aero.iitb.ac.in)
+#     Shivasubramanian G (IIT Bombay, sgopalak@iitb.ac.in)
+#------------------------------------------------------------------------------
+# License
+#
+#     This file is part of reynolds-blender.
+#
+#     reynolds-blender is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     reynolds-blender is distributed in the hope that it will be useful, but
+#     WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#     Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with reynolds-blender.  If not, see <http://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------------
+
+bl_info = {
+    "name": "OpenFoam BlockMeshDict Add-On",
+    "description": "",
+    "author": "Deepak Surti",
+    "version": (0, 0, 1),
+    "blender": (2, 78, 0),
+    "location": "3D View > Tools",
+    "warning": "", # used for warning icon and text in addons panel
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Development"
+}
+
+if "bpy" in locals():
+    import importlib
+    importlib.reload(block_mesh_addon)
+else:
+    from . import block_mesh_addon
+
+import bpy
+
+def register():
+    block_mesh_addon.register()
+
+def unregister():
+    block_mesh_addon.unregister()
