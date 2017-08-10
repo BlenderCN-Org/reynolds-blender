ed8c35f1dd81ac32d6eb4ed266abe689c6415350
Deepak Surti <dmsurti@gmail.com>
2017-08-10 18:13:23 +0530

    Update api to test a foam tutorial

--- tests/cavity/test_cavity_tutorial.py
+++ tests/cavity/test_cavity_tutorial.py
@@ -1,11 +1,46 @@
+#------------------------------------------------------------------------------
+# Reynolds-Blender | The Blender add-on for Reynolds, an OpenFoam toolbox.
+#------------------------------------------------------------------------------
+# Copyright|
+#------------------------------------------------------------------------------
+#     Deepak Surti       (dmsurti@gmail.com)
+#     Prabhu R           (IIT Bombay, prabhu@aero.iitb.ac.in)
+#     Shivasubramanian G (IIT Bombay, sgopalak@iitb.ac.in)
+#------------------------------------------------------------------------------
+# License
+#
+#     This file is part of reynolds-blender.
+#
+#     reynolds-blender is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     reynolds-blender is distributed in the hope that it will be useful, but
+#     WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#     Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with reynolds-blender.  If not, see <http://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------------
+
+# -----------
+# bpy imports
+# -----------
+import bpy, bmesh
+
+# --------------
+# python imports
+# --------------
 import os
 import pathlib
 import unittest
 
-import reynolds_blender
-
-import bpy, bmesh
-
+# ------------------------
+# reynolds_blender imports
+# ------------------------
+from reynolds_blender import bl_info
 from tests.foam_test_case import TestFoamTutorial
 
 class TestCavityTutorial(TestFoamTutorial):
@@ -14,132 +49,31 @@ class TestCavityTutorial(TestFoamTutorial):
         self.tutorial_name = 'cavity'
         self.test_module_dir = 'cavity'
         self.copy_tutorial_case_dir(self.tutorial_name, self.test_module_dir)
+        self.scene = bpy.context.scene
 
     def test_blockmesh_with_cavity_tutorial(self):
-        # test if addon got loaded correctly
-        # every addon must provide the "bl_info" dict
-        self.assertIsNotNone(reynolds_blender.bl_info)
-
-        # self.copy_tutorial_case_dir();
-
-        # -------------
-        # get the scene
-        # -------------
-        scene = bpy.context.scene
-
         # --------------
-        # start openfoam
+        # Initialization
         # --------------
-        bpy.ops.reynolds.start_of()
-
+        self.check_addon_loaded()
+        self.start_openfoam()
+        obj = self.scene.objects['Plane']
+        self.switch_to_edit_mode(obj)
+        self.select_case_dir('//cavity')
         # -------------------
-        # switch to edit mode
+        # Steps to solve case
         # -------------------
-        obj = scene.objects['Plane']
-        scene.objects.active = obj
-        bpy.ops.object.mode_set(mode='EDIT', toggle=False)
-
-        # --------------------
-        # select case directory
-        # --------------------
-        # use blender path format, the addon converts to abs path
-        # // means current dir in blender path notation
-        scene.case_dir_path = '//cavity'
-
-        # ------------------
-        # set convertToMeters
-        # -------------------
-        scene.convert_to_meters = 0.1
-
-        # -------------------------------------------------------------
-        # assign vertices with indices: 0, 1, 3, 2, 4, 5, 7, 6 in order
-        # -------------------------------------------------------------
-        bpy.ops.mesh.select_all(action='DESELECT')
-
-        # ------------------------------------------------
-        # Why is ensure_lookup_table() needed?
-        # See https://developer.blender.org/rB785b90d7efd0
-        # ------------------------------------------------
-
-        bpy.ops.mesh.select_mode(type='VERT')
-        for idx in  [0, 1, 3, 2, 4, 5, 7, 6]:
-            mesh = bmesh.from_edit_mesh(obj.data)
-            mesh.verts.ensure_lookup_table()
-            vertices = mesh.verts
-            vertices[idx].select = True
-            bpy.ops.vertices.list_action('EXEC_DEFAULT', action='ADD')
-            bpy.ops.reynolds.assign_vertex()
-            mesh = bmesh.from_edit_mesh(obj.data)
-            mesh.verts.ensure_lookup_table()
-            vertices = mesh.verts
-            vertices[idx].select = False
-
-        # -----------------------------------------------
-        # select all vertices in edit mode, assign blocks
-        # -----------------------------------------------
-        bpy.ops.mesh.select_all(action='SELECT')
-        bpy.ops.reynolds.blocks()
-        bpy.ops.mesh.select_all(action='DESELECT')
-
-        # -------------------
-        # set number of cells
-        # -------------------
-        scene.n_cells[0] = 20
-        scene.n_cells[1] = 20
-        scene.n_cells[2] = 1
-
-        # -------------------------
-        # set cell expansion ratios
-        # -------------------------
-        scene.n_grading[0] = 1
-        scene.n_grading[1] = 1
-        scene.n_grading[2] = 1
-
-        # -----------------------------------------------------------------
-        # set boundary
-        # 1. select face with index 4 as movingWall, set name, type
-        # 2. select face with index 3, 5, 2 as fixedWalls, set name, type
-        # 3. select faces with indices 0, 1 as frontAndBack, set name, type
-        # -----------------------------------------------------------------
-        bpy.ops.mesh.select_mode(type='FACE')
-        patches = {'movingWall': ([4], 'wall'),
-                    'fixedWalls': ([3, 5, 2], 'wall'),
-                    'frontAndBack': ([0, 1], 'empty')}
-        for name, (faces, type) in patches.items():
-            scene.region_name = name
-            scene.region_type = type
-            mesh = bmesh.from_edit_mesh(obj.data)
-            mesh.faces.ensure_lookup_table()
-            for f in faces:
-                mesh.faces[f].select = True
-            bpy.ops.regions.list_action('INVOKE_DEFAULT', action='ADD')
-            bpy.ops.reynolds.assign_region()
-            mesh = bmesh.from_edit_mesh(obj.data)
-            mesh.faces.ensure_lookup_table()
-            for f in faces:
-                mesh.faces[f].select = False
-
-        # ----------------------
-        # generate blockMeshDict
-        # ----------------------
-        bpy.ops.reynolds.generate_bmd()
-
-        # ---------------------
-        # run blockMesh command
-        # ---------------------
-        bpy.ops.reynolds.block_mesh_runner()
-
-        # ---------------
-        # set solver name
-        # ---------------
-        scene.solver_name = 'icoFoam'
-
-        # ----------
-        # solve case
-        # ----------
-        bpy.ops.reynolds.solve_case()
-
-        self.assertTrue(scene.case_solved)
+        self.scene.convert_to_meters = 0.1
+        self.select_vertices(obj, [0, 1, 3, 2, 4, 5, 7, 6])
+        self.set_number_of_cells(20, 20, 1)
+        self.set_grading(1, 1, 1)
+        self.assign_blocks()
+        self.select_boundary(obj)
+        self.generate_blockmeshdict()
+        self.run_blockmesh()
+        self.scene.solver_name = 'icoFoam'
+        self.solve_case();
+        self.assertTrue(self.scene.case_solved)
 
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestCavityTutorial)
 unittest.TextTestRunner().run(suite)
--- tests/foam_test_case.py
+++ tests/foam_test_case.py
@@ -25,10 +25,23 @@
 #     along with reynolds-blender.  If not, see <http://www.gnu.org/licenses/>.
 #------------------------------------------------------------------------------
 
+# ----------
+# bpy imports
+# -----------
+import bpy, bmesh
+
+# --------------
+# python imports
+# --------------
 from distutils.dir_util import copy_tree, remove_tree
 import os
 import pathlib
 import unittest
+
+# ------------------------
+# reynolds_blender imports
+# ------------------------
+from reynolds_blender import bl_info
 
 class TestFoamTutorial(unittest.TestCase):
     def setUp(self):
@@ -47,6 +60,96 @@ class TestFoamTutorial(unittest.TestCase):
         case_dir = os.path.join(tests_parent_dir, 'tests', 'tutorials',
                                 self.tutorial_name)
         copy_tree(case_dir, self.temp_tutorial_dir)
+
+    def start_openfoam(self):
+        bpy.ops.reynolds.start_of()
+
+    def generate_blockmeshdict(self):
+        bpy.ops.reynolds.generate_bmd()
+
+    def run_blockmesh(self):
+        bpy.ops.reynolds.block_mesh_runner()
+
+    def solve_case(self):
+        bpy.ops.reynolds.solve_case()
+
+    def set_number_of_cells(self, x, y, z):
+        self.scene.n_cells[0] = x
+        self.scene.n_cells[1] = y
+        self.scene.n_cells[2] = z
+
+    def set_grading(self, x, y, z):
+        self.scene.n_grading[0] = x
+        self.scene.n_grading[1] = y
+        self.scene.n_grading[2] = z
+
+    # ----------------------------
+    # assign vertices with indices
+    # -----------------------------
+    def select_vertices(self, obj, indices):
+        bpy.ops.mesh.select_all(action='DESELECT')
+
+        # ------------------------------------------------
+        # Why is ensure_lookup_table() needed?
+        # See https://developer.blender.org/rB785b90d7efd0
+        # ------------------------------------------------
+
+        bpy.ops.mesh.select_mode(type='VERT')
+        for idx in  indices:
+            mesh = bmesh.from_edit_mesh(obj.data)
+            mesh.verts.ensure_lookup_table()
+            vertices = mesh.verts
+            vertices[idx].select = True
+            bpy.ops.vertices.list_action('EXEC_DEFAULT', action='ADD')
+            bpy.ops.reynolds.assign_vertex()
+            mesh = bmesh.from_edit_mesh(obj.data)
+            mesh.verts.ensure_lookup_table()
+            vertices = mesh.verts
+            vertices[idx].select = False
+
+    def assign_blocks(self):
+        bpy.ops.mesh.select_all(action='SELECT')
+        bpy.ops.reynolds.blocks()
+        bpy.ops.mesh.select_all(action='DESELECT')
+
+    # -----------------------------------------------------------------
+    # set boundary
+    # 1. select face with index 4 as movingWall, set name, type
+    # 2. select face with index 3, 5, 2 as fixedWalls, set name, type
+    # 3. select faces with indices 0, 1 as frontAndBack, set name, type
+    # -----------------------------------------------------------------
+    def select_boundary(self, obj):
+        bpy.ops.mesh.select_mode(type='FACE')
+        patches = {'movingWall': ([4], 'wall'),
+                    'fixedWalls': ([3, 5, 2], 'wall'),
+                    'frontAndBack': ([0, 1], 'empty')}
+        for name, (faces, type) in patches.items():
+            self.scene.region_name = name
+            self.scene.region_type = type
+            mesh = bmesh.from_edit_mesh(obj.data)
+            mesh.faces.ensure_lookup_table()
+            for f in faces:
+                mesh.faces[f].select = True
+            bpy.ops.regions.list_action('INVOKE_DEFAULT', action='ADD')
+            bpy.ops.reynolds.assign_region()
+            mesh = bmesh.from_edit_mesh(obj.data)
+            mesh.faces.ensure_lookup_table()
+            for f in faces:
+                mesh.faces[f].select = False
+
+    def switch_to_edit_mode(self, obj):
+        self.scene.objects.active = obj
+        bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+
+    def select_case_dir(self, dir_path):
+        # use blender path format, the addon converts to abs path
+        # // means current dir in blender path notation
+        self.scene.case_dir_path = dir_path
+
+    def check_addon_loaded(self):
+        # test if addon got loaded correctly
+        # every addon must provide the "bl_info" dict
+        self.assertIsNotNone(bl_info)
 
     def tearDown(self):
         if self.temp_tutorial_dir:
